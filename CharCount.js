ba4888802288845062909cdda8654ab765a03555
Susmita Dey <susmitadey475@gmail.com>
2022-04-02 11:56:51 +0530

    Create CharCount.js

---
+++ CharCount.js
@@ -0,0 +1,3 @@
+var tweet = prompt("Compose your tweet: ");
+var tweetCount = tweet.length;
+alert("You have written "+tweetCount+" characters. You have only "+(140-tweetCount)+" characters left.");

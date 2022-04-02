9e8d8847792c579bf74e9aa07a6190d35f8095b4
Susmita Dey <susmitadey475@gmail.com>
2022-04-02 12:57:36 +0530

    added edge case

--- StringCasing.js
+++ StringCasing.js
@@ -1,5 +1,5 @@
 // Change the first character of your name to Uppercase and print Hello {your_name}.
 
 var name = prompt("What is your name?")
-name = (name.slice(0,1)).toUpperCase() + name.slice(1,name.length);
+name = (name.slice(0,1)).toUpperCase() + (name.slice(1, name.length)).toLowerCase();
 alert("Hello "+ name);

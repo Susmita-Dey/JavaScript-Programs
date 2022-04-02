b0007bb73b629222b5847f32924edbd883998e32
Susmita Dey <susmitadey475@gmail.com>
2022-04-02 13:06:47 +0530

    added parentheses

--- DogAgeHumanAge.js
+++ DogAgeHumanAge.js
@@ -1,5 +1,5 @@
 // Calculate your dog's age to a human age.
 
 dogAge = prompt("What's the age of your dog?")
-humanAge = (dogAge - 2) * 4 + 21;
-alert(humanAge);
+humanAge = ((dogAge - 2) * 4) + 21;
+alert("Your dog is "+ humanAge + " years old in human years");

96561b674ab10cde1bb007aa7f6c293f537fed23
Susmita Dey <susmitadey475@gmail.com>
2022-04-03 09:35:51 +0530

    Update BMI_Calculaator.js

--- BMI_Calculaator.js
+++ BMI_Calculaator.js
@@ -1,11 +1,11 @@
 //Create your function below this line.
 //The first parameter should be the weight and the second should be the height.
 function bmiCalculator(weight, height){
-    var bmi = weight/(height*height);
+    var bmi = weight / Math.pow(height, 2);
     return bmi;
 }
 
-console.log("Your bmi is: "+ Math.floor(bmiCalculator(65, 1.8)));
+console.log("Your bmi is: "+ Math.round(bmiCalculator(65, 1.8)));
 
 /* If my weight is 65Kg and my height is 1.8m, I should be able to call your function like this:
 

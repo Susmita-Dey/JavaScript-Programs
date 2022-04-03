c5d4d5dcd3f63d3e3b832b87341ed3cee3a22e51
Susmita Dey <susmitadey475@gmail.com>
2022-04-03 11:22:10 +0530

    Create BMI_Calculator1.js

---
+++ BMI_Calculator1.js
@@ -0,0 +1,15 @@
+function bmiCalculator (weight, height) {
+    var bmi = weight/Math.pow(height,2);
+    if(bmi<18.5){
+        var interpretation = "Your BMI is "+bmi+", so you are underweight.";
+    }
+    if(bmi>=18.5 && bmi<=24.9){
+        interpretation = "Your BMI is "+bmi+", so you have a normal weight.";
+    }
+    if(bmi>24.9){
+        interpretation = "Your BMI is "+bmi+", so you are overweight.";
+    }
+    return interpretation;
+}
+
+bmiCalculator (85, 2);

9c3cf23755a1c946711e3eb5c2be59ed7ac567b5
Susmita Dey <susmitadey475@gmail.com>
2022-04-03 12:37:02 +0530

    Create FizzBuzz_While.js

---
+++ FizzBuzz_While.js
@@ -0,0 +1,21 @@
+var output=[];
+var count=1;
+
+function fizzBuzz() {
+    // Write your code here.
+   while(count<=100){
+     if(count%3==0 && count%5==0){
+        output.push("FizzBuzz")
+    }else if(count%3===0){
+        output.push("Fizz");
+    } else if(count%5===0){
+        output.push("Buzz");
+    } else {
+        output.push(count);
+    }
+    count++;
+   }
+    console.log(output);
+}
+
+fizzBuzz();

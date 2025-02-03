0c78d5cb6d54c1ffe33bd510a1c991806aec45c3
Susmita Dey <susmitadey475@gmail.com>
2025-02-03 17:01:29 +0530

    Add files via upload

---
+++ Encourage-Bot/index.js
@@ -0,0 +1,40 @@
+// Require the necessary DiscordAPIError.js classes
+const fs = require("node:fs");
+const path = require("node:path");
+const { Client, Collection, Intents, MessageEmbed } = require("discord.js");
+const { token } = require("./config.json");
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  // Set a new item in the collection
+  // With the key as the command name and the value as the exported module
+  client.commands.set(command.data.name, command);
+}
+
+const eventsPath = path.join(__dirname, "events");
+const eventFiles = fs
+  .readdirSync(eventsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
+}
+
+// Login to Discord with your client's token
+client.login(token);
